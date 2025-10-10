fb304eeec91f201b1c9d799e1119258f3c8f13cf
Nishiki236 <t246003@st.yamagata-u.ac.jp>
2025-10-10 19:10:16 +0900

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    /**名前を付けて保存をしておく**/
+     int aaaa = 6003;
+     printf("プログラミング%d \n ",aaaa);
+     aaaa = (aaaa-1)/2;
+    /**printfは""の間に表記したい文字を書くよ**/
+    printf("プログラミング%d",aaaa);
+
+    return 0;
+}
